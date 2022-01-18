3ad204feb993ee3a800b384308feaa39888ab329
ijo0r98 <ijo0r98@naver.com>
2022-01-18 22:12:27 +0900

    코딩테스트 레퍼지토리 합침

--- programmers/ex/filter.py
+++
@@ -1,3 +0,0 @@
-target = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-result = filter(lambda x : x % 2==0, target)
-print(list(result))
--- programmers/ex/거스름돈.py
+++
@@ -1,9 +0,0 @@
-n = int(input())
-cnt = 0
-lst=[500,100,50,10] # 거스름돈 종류
-
-for i in lst:
-  cnt += n // i
-  n %= i
-
-print(cnt)
